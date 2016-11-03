4899684e56db3bfaa716031f1f66587fbb43e7a3
yasong <752198313@qq.com>
2016-11-03 10:49:56 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -50,3 +50,83 @@ def main():
             
     server_loop(local_host,local_port,remote_host,remote_port,receive_first)
 main()
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host,remote_port))
+    
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+        
+        remote_buffer = response_handler(remote_buffer)
+        
+        if len(remote_buffer):
+            print "[<==] Sending %d bytes to localhost." % len(remote_buffer)
+            client_socket.send(remote_buffer)
+            
+        while True:
+            
+            local_buffer = receive_from(client_socket)
+            
+            if len(local_buffer):
+                print "[==>] Received %d bytes from localhost." % len(local_buffer)
+                hexdump(local_buffer)
+                local_buffer = request_handler(local_buffer)
+            
+                remote_socket.send(local_buffer)
+                print "[==>] Send to remote."
+            remote_buffer = receive_from(remote_socket)
+            
+            if len(remote_buffer):
+                print "[<==] Received %d bytes from remote." % len(remote_buffer)
+                hexdump(remote_buffer)
+                
+                remote_buffer = response_handler(remote_buffer)
+                
+                client_socket.send(remote_buffer)
+                
+                print "[<==] Sent to localhost."
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print "[*] No more data. Closing connections."
+                break
+                
+def hexdump(src, length=16):
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+    
+    for i in xrange(0, len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b''. for x in s])
+        result.append(b"%04X    %-*s    %s" % (i, lenght*(digits + 1), hexa, text))
+        
+    print b'\n'.joi(result)
+def receive_from(connection):
+    
+    buffer = ""
+    
+    connection.settimeout(2)
+    
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except:
+        pass
+    return buffer
+def request_handler(buffer):
+    #change teh packets
+    return buffer
+
+def response_handler(buffer):
+    #change the packets
+    return buffer.
+
+            
+           
