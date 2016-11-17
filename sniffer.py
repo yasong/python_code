b1031fbf79e9465c5680e07813975a5bc6b04db0
yasong <752198313@qq.com>
2016-11-17 11:18:17 +0800

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,38 @@
 import socket
 import os
+import struct
+from ctypes import *
 
+class IP(Structure):
+    _fields_ = [
+        ("ihl",         c_ubyte, 4),
+        ("version",     c_ubyte, 4),
+        ("tos",         c_ubyte),
+        ("len",         c_ushort),
+        ("id",          c_ushort),
+        ("offset",      c_ushort),
+        ("ttl",         c_ubyte),
+        ("protocol_num",c_ubyte),
+        ("sum",         c_ushort),
+        ("src",         c_ulong),
+        ("dst",         c_ulong)
+    ]
+
+def __new__(self, socket_buffer=None):
+    return self.from_buffer_copy(socket_buffer)
+
+def __init__(self,socket_buffer=None):
+    self.protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
+
+    self.src_address = socket.inet_ntoa(struct.pack("<L",self.src))
+    self.dst_address = socket.inet_ntoa(struct.pack("<L",self.dst))
+
+    try:
+        self.protocol = self.protocol_map[self.protocol_num]
+    except:
+        self.protocol =str(self.protocol_num)
 #
-host = "192.168.0.196"
+host = "10.104.171.27"
 
 #
 if os.name == "nt":
@@ -20,7 +50,21 @@ sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 if os.name == "nt":
 	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-print sniffer.recvfrom(65565)
+try:
+    while True:
 
-if os.name == "nt":
-	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        #read packets
+        raw_buffer = sniffer.recvfrom(65565)[0]
+        print raw_buffer[0:20]
+        #if type(raw_buffer[0:20]) == "int":
+        
+        ip_header =IP(raw_buffer[0:20])
+        
+
+        print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+
+        print raw_buffer
+except KeyboardInterrupt:
+
+        if os.name == "nt":
+                sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
