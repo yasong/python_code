23f0ab94a535c338eac6d474db1bbfad0d7daa26
Zx0ky <yasongkangkang@gmail.com>
2017-12-26 11:02:10 +0800

    Update icmp.py

--- icmp.py
+++ icmp.py
@@ -6,7 +6,27 @@ import datetime
 import socket
 import struct
 
+eth_src = "\x00\x00\x00\x00\x00\x00"
+eth_dst = "\x00\x00\x00\x00\x00\x00"
+eth_type = 0x800
 
+ip_src = "192.168.1.1."
+ip_dst = "192.168.1.110"
+
+def checksum(packet):
+    if (len(bData) & 1):
+        bData += b'\x00';
+    i = 0;
+    nSum = 0;
+    while i < len(bData):
+        nSum += (bData[i] << 8) + bData[i+1];
+        i += 2;
+    nCarry = nSum >> 16;
+    while nCarry:
+        nSum = (nSum & 0xffff) + nCarry;
+        nCarry = (nSum >> 16);
+    nSum = (~nSum & 0xffff);
+    return nSum;
 
 def main():
     
