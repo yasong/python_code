46191829f0c5096583fe24f121e86cf537c65fb7
Zx0ky <yasongkangkang@gmail.com>
2017-12-26 11:05:39 +0800

    Update icmp.py

--- icmp.py
+++ icmp.py
@@ -9,9 +9,26 @@ import struct
 eth_src = "\x00\x00\x00\x00\x00\x00"
 eth_dst = "\x00\x00\x00\x00\x00\x00"
 eth_type = 0x800
+eth_len = 14
+overflow_len = 
+
 
 ip_src = "192.168.1.1."
 ip_dst = "192.168.1.110"
+options = "\x00\x83\x94\x04"
+
+#ip header
+ip_ver = 4
+ip_ihl = 6
+ip_tos = 0
+ip_total_len = 
+ip_id = 0
+ip_frag_offset = 0
+ip_protocol = socket.IPPROTO_ICMP
+ip_checksum = 0
+ip_src_addr = socket.inet_pton(socket.AF_INET, ip_src)
+ip_dst_addr = socket.inet_pton(socket.AF_INET, ip_dst)
+
 
 def checksum(packet):
     if (len(bData) & 1):
